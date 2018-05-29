25ada589ef532315c54e326f4f837b8b5388b2a6
root <root@linux-jsmu.suse>
2018-05-29 17:54:56 +0530

    first commit

---
+++ ffms/src/main/java/com/hm/ffms/ffms/FfmsApplication.java
@@ -0,0 +1,12 @@
+package com.hm.ffms.ffms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfmsApplication.class, args);
+	}
+}
